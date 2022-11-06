dbce5781539e04bffea6a8a27108cc492be22d4a
Pawel Paczkowski <paw.pac@yahoo.com>
2022-11-06 13:24:18 -0600

    Add altitude increments

---
+++ script.js
@@ -0,0 +1,10 @@
+// const skydiver = document.querySelector('.skydiver');
+
+// let margin = 0;
+
+// setInterval(() => {
+//   skydiver.style.marginTop = margin + .12 + 'px';
+//   margin = margin + 0.083;
+// }, 10);
+
+
