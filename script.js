ad0824ca71f30bcf1aea5bb782c33d362386c8af
Pawel Paczkowski <paw.pac@yahoo.com>
2022-11-07 15:55:20 -0600

    Add and rig up exit and pull altitude menus

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
-// const skydiver = document.querySelector('.skydiver');
+const altitudeLoss = document.querySelector('.altitude-loss');
 
-// let margin = 0;
-
-// setInterval(() => {
-//   skydiver.style.marginTop = margin + .12 + 'px';
-//   margin = margin + 0.083;
-// }, 10);
+let loss = 0;
+let altitudeDrop = 0;
 
 
+
+//CONTROLS
+
+const exitAlt = document.getElementById('exit-altitude');
+const pullAlt = document.getElementById('pull-altitude');
+
+const go = document.getElementById('go');
+
+let exitChoice = exitAlt.value;
+let pullChoice = pullAlt.value;
+
+go.addEventListener('click', () => {
+  exitChoice = exitAlt.value;
+  pullChoice = pullAlt.value;
+
+  loss = 100 - (exitChoice * 6.25) - 1.06282999;
+
+  const dropAlittude = setInterval(() => {
+    altitudeDrop = loss + 1.1;
+    altitudeLoss.style.height = altitudeDrop + '%';
+    loss = loss + 0.011;
+
+
+
+    if ((100 - (pullChoice * 6.25) - 1.06282999) < altitudeDrop) {
+      clearInterval(dropAlittude);
+    }
+
+
+
+
+  }, 10);
+
+
+
+
+});
